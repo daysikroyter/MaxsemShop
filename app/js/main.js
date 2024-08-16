a287304ec90b17809b77618675dc8e4f78d48287
daysikroyter <nubchenko@gmail.com>
2024-08-16 15:55:03 +0300

    added responsive section products & explore

--- app/js/main.js
+++ app/js/main.js
@@ -66,4 +66,66 @@ $(function () {
     autoplay: true
   });
 
+  $('.tab').on('click', function (e) {
+    e.preventDefault();
+
+    const $tabs = $(this).closest('.tabs');
+    const index = $(this).parent().index();
+
+    $tabs.find('.tab--active').removeClass('tab--active');
+    $(this).addClass('tab--active');
+
+    const $contents = $tabs.next('.tabs-container').find('.tabs-content');
+    $contents.removeClass('tabs-content--active');
+    $contents.eq(index).addClass('tabs-content--active');
+  });
+
+
+  $(document).on('click', '.product-card__favorite', function () {
+    $(this).toggleClass('product-card__favorite--active');
+  });
+
+  $('.showcase__list').slick({
+    slidesToShow: 4,
+    autoplay: true,
+    prevArrow: '<button type="button" class="slick-arrow slick-prev"><svg class="arrow"><use xlink:href="images/icons/sprite/sprite.svg#prev-arrow"></use></svg></button>',
+    nextArrow: '<button type="button" class="slick-arrow slick-next"><svg class="arrow"><use xlink:href="images/icons/sprite/sprite.svg#prev-arrow"></use></svg></button>',
+    responsive: [
+      {
+        breakpoint: 1320,
+        settings: {
+          dots: true,
+          arrows: false
+        }
+      },
+      {
+        breakpoint: 1050,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          dots: true,
+          arrows: false
+        }
+      },
+      {
+        breakpoint: 860,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          dots: true,
+          arrows: false
+        }
+      },
+      {
+        breakpoint: 561,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: true,
+          arrows: false
+        }
+      },
+    ]
+  });
+
 });
